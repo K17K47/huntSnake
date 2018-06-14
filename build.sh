347f343542f8d8c18940960e6eb8a93f75d3e703
K17K47 <k17k47@gmail.com>
2018-06-14 14:04:19 -0300

    Adicionados arquivos base para inteligencia artificial

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ gcc -c aux.c -o aux.o
 gcc -c ui.c -o ui.o
 gcc -c logic.c -o logic.o
 gcc -c menus.c -o menus.o
+gcc -c ai.c -o ai.o
 gcc -c main.c -o main.o
-gcc ui.o aux.o logic.o menus.o main.o -o huntSnake
-rm ui.o aux.o logic.o main.o menus.o
+gcc ui.o aux.o logic.o menus.o ai.o main.o -o huntSnake
+rm ui.o aux.o logic.o main.o menus.o ai.o
